c771de1e33e302568bf11b4e24d59635f70c8d18
jrmosedale <J.Mosedale@exeter.ac.uk>
2018-10-02 11:45:55 +0100

    New version testing (i) Inclusion of country wide rasters (ii) loading of leaflet widget via iframe (iii) Use of bsselectR package for dynamic menus

---
+++ Rscripts/create_Zprotection_mapfiles.R
@@ -0,0 +1,120 @@
+
+library(rgdal)
+library(rgeos)
+library(raster)
+library(ggplot2)
+library(sf)
+library(mapview)
+library(RColorBrewer)
+library(leaflet)
+library(zonator)
+library(htmlwidgets)
+
+###############################################################
+# Set directory to write to
+###############################################################
+#dir_out<-paste0(dir_o)
+
+
+
+###############################################################
+# FUNCTIONS & DIRECTORIES
+###############################################################
+
+# Recalculating rank after masking
+rank_after_mask<-function(r,msk){
+  msk.cells<-getValues(mask(r,msk))
+  min.prot<-min(msk.cells,na.rm=TRUE)
+  # num.prot<-length(msk.cells[-which(is.na(msk.cells))])
+  rem.cells<-getValues(mask(r,msk,inverse=TRUE))
+  max.rem<-max(rem.cells,na.rm=TRUE)
+  new.ranks<-(rem.cells/max.rem)*1
+  new.r<-setValues(r,new.ranks)
+  return(new.r)
+}
+
+rank_after_crop<-function(r,local.r){
+  local.cells<-getValues(local.r)
+  min.local<-min(local.cells,na.rm=TRUE)
+  max.local<-max(local.cells,na.rm=TRUE)
+  # num.prot<-length(msk.cells[-which(is.na(msk.cells))])
+  all.cells<-getValues(r)
+  min.all<-min(all.cells,na.rm=TRUE)
+  max.all<-max(all.cells,na.rm=TRUE)
+  
+  new.ranks<-(local.cells/(max.local-min.local))*(max.all-min.all)
+  new.r<-setValues(local.r,new.ranks)
+  return(new.r)
+}
+
+pal <- colorRampPalette(brewer.pal(10, "BrBG"))
+pal <- colorRampPalette(brewer.pal(9, "BuGn"))
+leg<-zlegend("spectral")
+
+###############################################################
+# Choose Zproject
+###############################################################
+### SET PROJECT PATH ###
+dir_ZProject<-"D:/Zonation_Projects/Cornwall_Priority/"
+# or if on portable drive
+dir_ZProject<-paste0("/Volumes/Pocket_Sam/data/Zonation_Projects/Cornwall_Priority/")
+
+dir_zinputs<-"/Volumes/Pocket_Sam/data/Zonation_data/"
+###############################################################
+### CHOOSE VARIANT ###
+###############################################################
+
+v<-"v15" # Applies connectivity matrix
+
+
+###############################################################
+# Analyse Variant Results
+###############################################################
+# RANKING MAP
+r<-raster(paste0(dir_ZProject,v,"/",v,"_out/",v,"_output.ABF_MBLP10.rank.compressed.tif"))
+#plot(r,breaks=leg$values, col=leg$colors, main=v)
+
+# Masking - priority
+cornwall.r<-raster(paste0(dir_zinputs,"cornwall_LT.tif"))
+protected.r<-raster(paste0(dir_zinputs,"protected_mask.tif"))
+protected.r<-mask(protected.r,cornwall.r)
+plot(protected.r)
+plot(mask(r,protected.r,inverse=TRUE),breaks=leg$values, col=leg$colors)
+priority.r<-raster(paste0(dir_zinputs,"desig_priority.tif"))
+
+new.r<-rank_after_mask(r,protected.r)
+
+plot(new.r,breaks=leg$values, col=leg$colors, main="New ranking without existing protected areas")
+cws.r<-raster(paste0(dir_zinputs,"desig_cws.tif"))
+plot(cws.r,add=T,col="purple")
+plot(zero_to_NA(priority.r),col="green",add=T)
+
+# Extract Top Ranked Areas
+toprank5<-calc(new.r,fun=function(x){ifelse(x>=0.95,1,NA)})
+plot(toprank5,col="red")
+toprank10<-calc(new.r,fun=function(x){ifelse(x>=0.9 & x<0.95,1,NA)})
+plot(toprank10,add=T,col="orange")
+toprank20<-calc(new.r,fun=function(x){ifelse(x>=0.8 & x < 0.9,1,NA)})
+plot(toprank20,add=T,col="brown")
+
+###############################################################
+# WRITE MAP FILE
+###############################################################
+
+zmap<-leaflet() %>%
+  setView(lng = -4.9, lat = 50.4, zoom = 10)  %>%
+  addProviderTiles("OpenStreetMap.Mapnik") %>%
+  addScaleBar() %>%
+  addRasterImage(protected.r, colors = "yellow", opacity = 0.7,group="Protected areas") %>%
+  addRasterImage(cws.r, colors = "purple", opacity = 0.5,group="County Wildlife Sites") %>%
+  addRasterImage(zero_to_NA(priority.r), colors = "green", opacity = 0.5,group="Priority habitats") %>%
+  addRasterImage(toprank5, colors = "red", opacity = 0.7,group="Top 5%") %>%
+  addRasterImage(toprank10, colors = "orange", opacity = 0.7,group="Top 10%") %>%
+  addRasterImage(toprank20, colors = "brown", opacity = 0.7,group="Top 20%") %>%
+  addLayersControl(overlayGroups = c("Protected areas","Priority habitats", "County Wildlife Sites","Top 5%","Top 10%","Top 20%"),
+                   options = layersControlOptions(collapsed=FALSE) ) %>% 
+  hideGroup(c("Priority habitats", "County Wildlife Sites"))
+
+filename<-paste0(getwd(),"/maps/opps/opp_nogo_map.html")
+print(filename)
+saveWidget(zmap,file=filename)
---
+++ Rscripts/create_cnarea_mapfiles.R
@@ -0,0 +1,25 @@
+# Create Community Network Area maps
+
+# Load CNA shapefile data
+cna.shp<-st_read(paste0(in.root,"community_network_areas.shp"))
+st_crs(cna.shp)<-st_crs(27700)
+cna.shp<-st_transform(cna.shp,4326)
+cna.list<-paste0(cna.shp$NAME)
+names(cna.list)<-cna.list
+
+
+for (n in cna.list){
+  cna.map<-leaflet() %>% 
+    setView(lng = -4.9, lat = 50.4, zoom = 10)  %>%
+    addProviderTiles("OpenStreetMap.Mapnik") %>%
+    addScaleBar() %>%
+    addPolygons(data=cna.shp[which(cna.shp$NAME==n),], color="black",fill=FALSE)
+  mapname<-gsub(" ","_",n)
+  mapname<-gsub( "[[:punct:]]", "", mapname) 
+  #mapname<-paste0(dir_cna,"map",mapname,".html")
+  mapname<-paste0("map",mapname,".html")
+  print(mapname)
+  saveWidget(cna.map, file=mapname)
+}
+
+
